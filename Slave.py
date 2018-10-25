bebd01fc3f50a89683862463bfc51daea92800ec
Luke Welton <law0047@auburn.edu>
2018-10-25 13:32:39 -0500

    fix bryant's parenthesis deficiency

--- Slave.py
+++ Slave.py
@@ -8,7 +8,7 @@ def main(argv):
         master_hostname = argv[1]
         master_port = argv[2] # for us this should be 10075
     except:
-        print("Invalid arguments."
+        print("Invalid arguments.")
         sys.exit()
 
     # Send Join Ring Request
@@ -21,16 +21,16 @@ def main(argv):
     response = s.recv(4096)
 
     # Set self as slave node on ring and get ring ID
-    master_gid = int(str(response[0])
-    magic_number = int(str(response[1] + str(response[2] + str(response[3] + str(response[4])
-    thisRID = int(str(response[5])
-    nextSlaveIP = int str(response[6] + str(response[7] + str(response[8] + str(response[9]) 
-    nextSlave_pretty = str(response[6] + "." + str(response[7] + "." + str(response[8] + "." + str(response[9]
+    master_gid = int(str(response[0]))
+    magic_number = int(str(response[1]) + str(response[2]) + str(response[3]) + str(response[4]))
+    thisRID = int(str(response[5]))
+    nextSlaveIP = int(str(response[6]) + str(response[7]) + str(response[8]) + str(response[9]))
+    nextSlave_pretty = str(response[6]) + "." + str(response[7]) + "." + str(response[8]) + "." + str(response[9])
 
     # Display the GID of master, own ring ID, and the IP address of next slave
     print("Master GID: ", master_gid)
     print("My Ring ID: ", thisRID)
-    print("Next Slave: ", nextSlave_pretty
+    print("Next Slave: ", nextSlave_pretty)
 
     # TODO: Prompt user for a ring ID and message
 
